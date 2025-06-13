977080b585957ad3c44513d3cd551c10c6b6cf07
zapphyre <mmanco89@gmail.com>
2025-06-13 23:46:21 +0200

    timeouting gesture cb

--- src/main/java/org/zapphyre/fizzy/GesturizerAdapter.java
+++ src/main/java/org/zapphyre/fizzy/GesturizerAdapter.java
@@ -3,15 +3,21 @@ package org.zapphyre.fizzy;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import lombok.experimental.NonFinal;
+import lombok.extern.slf4j.Slf4j;
 import org.zapphyre.model.PolarCoords;
+import org.zapphyre.model.error.GestureTimeoutException;
 import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.MonoSink;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
+@Slf4j
 @Value
 @RequiredArgsConstructor
 public class GesturizerAdapter<T> {
@@ -22,8 +28,18 @@ public class GesturizerAdapter<T> {
     Disposable gestureCb = Mono::empty;
 
     public Consumer<MonoSink<String>> finishAfterFirst(Flux<T> polarStream) {
-        return q ->
-                gestureCb = gesturizer.pathCompose(polarStream.map(polarCoordsMapper)).gestureCb(q::success);
+        return q -> {
+            ScheduledFuture<?> timeout = Executors.newSingleThreadScheduledExecutor().schedule(() -> {
+                log.debug("Polar stream timeout");
+                q.error(new GestureTimeoutException("Gesture timeout after 4 sec"));
+            }, 4, TimeUnit.SECONDS);
+
+            q.onDispose(() -> timeout.cancel(true));
+
+            gestureCb = gesturizer
+                    .pathCompose(polarStream.map(polarCoordsMapper))
+                    .gestureCb(q::success);
+        };
     }
 
     public void dispose() {
---
+++ src/main/java/org/zapphyre/model/error/GestureTimeoutException.java
@@ -0,0 +1,8 @@
+package org.zapphyre.model.error;
+
+public class GestureTimeoutException extends RuntimeException {
+
+    public GestureTimeoutException(String message) {
+        super(message);
+    }
+}
